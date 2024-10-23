676e098f8224a51a50b7db879d0d67896c259332
Eduardo Decarli <decarliedu@gmail.com>
2024-10-23 11:20:21 -0300

    first commitgit

---
+++ decarli/src/test/java/com/api_rest/decarli/StartupTests.java
@@ -0,0 +1,13 @@
+package com.api_rest.decarli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
